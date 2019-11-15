ce2f8969a084ef9efab50a63300e3dcb199d98c6
DRLondon <dylond12@gmail.com>
2019-11-15 12:54:49 -0500

    Heavy changes

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-/*
 const express = require('express');
-const path = require('path');
+const app = express();
 const PORT = process.env.PORT || 5000;
+const morgan = require('morgan');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on port ${ PORT }`));
-*/
+app.use(morgan('combined'));
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app).listen(5000);
-var port = 5000;
+app.get("/", (req, res) => {
+	console.log("Responding to root route");
+	res.send("Hello from root.");
+});
 
-app.use("/css", express.static("./css"));
-app.use("/js", express.static("./js"));
-app.use("/img", express.static("./img"));
-app.use("/html", express.static("./html"));
-app.use("/php", express.static("./php"));
+app.get("/users", (req, res) => {
+	var user_one = {firstName: "Dyllon", lastName: "London"};
+	res.json([user_one]);
+});
 
-console.log(`Server started on port ${port}`);
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + '/html/index.ejs');
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });
